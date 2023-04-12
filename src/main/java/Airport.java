7dbacf1ba54105910922a88fc873573e6bd7d3f9
Intisar Sikder <sikder.intisar@gmail.com>
2023-04-12 13:59:00 +0100

    added method for cancelling flights and checking total number of passengers and flights.

--- src/main/java/Airport.java
+++ src/main/java/Airport.java
@@ -6,12 +6,18 @@ public class Airport {
 
         Scanner scanner = new Scanner(System.in);
         Passenger testPassenger = new Passenger(000,"blank", "blank");
-        Flight testFlight = new Flight("Destination", 12345, "blank");
+        Flight testFlight = new Flight("Destination", 12345);
         ArrayList<Flight> newFlightList = new ArrayList<Flight>();
+
+//        Creating a while loop. Adding a destination
+
         System.out.println("Welcome to BNTA Airport! Please enter your flight destination");
         while (true){
             String inputDestination = scanner.nextLine();
             testFlight.setDestination(inputDestination);
+
+//          Adding flight ID
+
             System.out.println("Thanks for sharing! Now, how about a flight ID?");
             String inputFlightID = scanner.nextLine();
             testFlight.setFlightID(Integer.parseInt(inputFlightID));
@@ -19,9 +25,8 @@ public class Airport {
             System.out.println("Cheers, we've got that!");
             System.out.println("Your destination is " + testFlight.getDestination() +
                     " and your flight ID is " + testFlight.getFlightID());
-// Displaying total flights
-            System.out.println("The total flights are: " + newFlightList);
 
+//             Adding passenger info
 
             System.out.println("What is your passenger's name?");
             String inputPassengerName = scanner.nextLine();
@@ -36,31 +41,38 @@ public class Airport {
                     + " your passenger ID is " + testPassenger.getId()
                     + " and your contact info is " + testPassenger.getContactInfo());
 
+//            Adding passenger to a flight
+
             System.out.println("Would you like to book on to this flight? Please select between Yes or No");
             String inputBooking = scanner.nextLine();
-            if(inputBooking.equals("Yes")) {
+            if(inputBooking.equalsIgnoreCase("Yes")) {
                 testFlight.addPassenger(testPassenger);
                 System.out.println("Congratulations, you are on the flight!");
             } else {
                 System.out.println("Sure, no problem.");
             }
 
-            System.out.println("The total flights are: " + newFlightList);
-//            System.out.println("What flight would you like to cancel?");
-//
-//
-//            String inputCancelledFlight = scanner.findInLine();
+//            Checking total numbers of passengers
+            System.out.println("Your current total number of passengers is: " + testFlight.getPassengers().size());
+
+//            Cancelling flight
+
+            System.out.println("Would you like to cancel the flight? Please select between Yes or No");
+            String inputCancelledFlight = scanner.nextLine();
+            if(inputCancelledFlight.equalsIgnoreCase("Yes")) {
+                newFlightList.remove(testFlight);
+                System.out.println("Great, you flight has been cancelled!");
+            } else {
+                System.out.println("Sure, no problem.");
+            }
+
+            // Checking number of flights
+            System.out.println("Your current total number of flights is: " + newFlightList.size());
 
             break;
             }
     }
 
-    }
 
-//    "Welcome to BNTA Airport! Please enter your flight destination. Please select one of the following 5 options:"
-//      1. Add a new flight
-//      2. Display all available flights
-//      3. Add a new passenger
-//      4. Book a passenger onto a flight
-//      5. Cancel a flight
+    }
 
--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,17 +1,12 @@
 import java.util.ArrayList;
 
 public class Flight {
-    // string destination
-    // int id
-    //arraylist (list of type PASSENGERS)
-    //getters and setters
-    //add a passenger to the class
     private String destination;
     private ArrayList<Passenger> passengerArrayList;
     private int flightID;
 
 
-    public Flight(String destination, int flightID, String passengerName){
+    public Flight(String destination, int flightID){
         this.destination = destination;
         this.flightID = flightID;
         this.passengerArrayList = new ArrayList<>();
@@ -22,8 +17,7 @@ public class Flight {
     public int getFlightID() { return this.flightID; }
     public ArrayList<Passenger> getPassengers() {return this.passengerArrayList; }
     public int getPassengerCount() { return this.passengerArrayList.size();}
-//    public ArrayList<Flight> getFlights() {return this.flightArrayList; }
-//    public int getFlightCount() { return this.flightArrayList.size();}
+
     //setters
 
     public void setDestination(String newDestination) {this.destination = newDestination;}
