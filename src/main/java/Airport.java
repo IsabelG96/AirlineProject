6c1eb1887fb600cd2a7c6498019e6e82f0534f9d
Isabel <isabelgalwey@gmail.com>
2023-04-12 09:58:05 +0100

    airport class

--- src/main/java/Airport.java
+++ src/main/java/Airport.java
@@ -6,10 +6,17 @@ public class Airport {
         Scanner scanner = new Scanner(System.in);
         Flight testFlight = new Flight("Destination", 12345);
         System.out.println("Welcome to BNTA Airport! Please enter your flight destination");
+        while (true){
+            String inputDestination = scanner.nextLine();
+            testFlight.setDestination(inputDestination);
+            System.out.println("Thanks for sharing! Now, how about a flight ID?");
+            String inputFlightID = scanner.nextLine();
+            testFlight.setFlightID(Integer.parseInt(inputFlightID));
+            System.out.println("Cheers, we've got that!");
+            System.out.println("Your destination is" + testFlight.getDestination() "and your flight ID is" );
+            }
 
-        
+        }
+
     }
 
-
-
-}
--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -7,24 +7,24 @@ public class Flight {
     //getters and setters
     //add a passenger to the class
     private String destination;
-    private ArrayList<Passenger> passengerArrayList;
+//    private ArrayList<Passenger> passengerArrayList;
     private int flightID;
 
     public Flight(String destination, int flightID){
         this.destination = destination;
         this.flightID = flightID;
-        this.passengerArrayList = new ArrayList<Passenger>();
+//        this.passengerArrayList = new ArrayList<Passenger>();
     }
     //getters
     public String getDestination() { return this.destination; }
     public int getFlightID() { return this.flightID; }
-    public ArrayList<Passenger>() getPassengers() {return this.passengerArrayList; }
-    public int getPassengerCount() { return this.passengerArrayList.size();}
+//    public ArrayList<Passenger>() getPassengers() {return this.passengerArrayList; }
+//    public int getPassengerCount() { return this.passengerArrayList.size();}
     //setters
 
     public void setDestination(String newDestination) {this.destination = newDestination;}
     public void setFlightID(int newFlightID) {this.flightID = newFlightID;}
-    public void addPassenger(Passenger newPassenger) {this.passengerArrayList.add(newPassenger);}
+//    public void addPassenger(Passenger newPassenger) {this.passengerArrayList.add(newPassenger);}
 
 
 }
