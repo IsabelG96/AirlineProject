5794a7785ca2892926c634c884afffc0eb704c29
Isabel <isabelgalwey@gmail.com>
2023-04-12 11:02:42 +0100

    added flights array list

--- src/main/java/Airport.java
+++ src/main/java/Airport.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Airport {
@@ -5,6 +6,7 @@ public class Airport {
 
         Scanner scanner = new Scanner(System.in);
         Flight testFlight = new Flight("Destination", 12345);
+        ArrayList<Flight> newFlightList = new ArrayList<Flight>();
         System.out.println("Welcome to BNTA Airport! Please enter your flight destination");
         while (true){
             String inputDestination = scanner.nextLine();
@@ -12,9 +14,11 @@ public class Airport {
             System.out.println("Thanks for sharing! Now, how about a flight ID?");
             String inputFlightID = scanner.nextLine();
             testFlight.setFlightID(Integer.parseInt(inputFlightID));
+            newFlightList.add(testFlight);
             System.out.println("Cheers, we've got that!");
             System.out.println("Your destination is " + testFlight.getDestination() +
                     " and your flight ID is " + testFlight.getFlightID());
+            System.out.println("The total flights are: " + newFlightList);
             }
 
         }
